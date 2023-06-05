dc1fb14e53af7a7aaeb953e250a2443ab5d54167
wolfhawk333 <wolfhawk747333@gmail.com>
2023-06-05 13:04:10 -0700

    5v1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - use printf to print string
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
