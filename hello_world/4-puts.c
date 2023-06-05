04848390974d9832a00ed51b0e24e52cce4910c9
wolfhawk333 <wolfhawk747333@gmail.com>
2023-06-05 12:49:29 -0700

    4v1

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - use puts to print string
+ * 
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
